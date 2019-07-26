da0ee6270d28ac2475c86a1b467d1789c6c15547
Graham Fairweather <xotic750@gmail.com>
2019-07-26 18:22:41 +0200

    :bookmark: v2.0.8
    Auto build

--- dist/object-walk-x.js
+++ dist/object-walk-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-2017",
-  "date": "2019-07-25T22:58:07.191Z",
+  "date": "2019-07-26T16:22:33.726Z",
   "describe": "",
   "description": "Walks a given object and invokes a function on each iteration.",
   "file": "object-walk-x.js",
   "hash": "1fc670b31ba790922633",
   "license": "MIT",
-  "version": "2.0.7"
+  "version": "2.0.8"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
