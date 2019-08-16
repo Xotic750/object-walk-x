193d7fc248c92c56b0fadb8677fb045de7612c8e
Graham Fairweather <xotic750@gmail.com>
2019-08-17 01:34:30 +0200

    Maintenance

--- dist/object-walk-x.js
+++ dist/object-walk-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-2017",
-  "date": "2019-08-15T20:55:06.878Z",
+  "date": "2019-08-16T23:34:23.022Z",
   "describe": "",
   "description": "Walks a given object and invokes a function on each iteration.",
   "file": "object-walk-x.js",
